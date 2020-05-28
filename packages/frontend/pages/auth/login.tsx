8d8138a129e95fc6282b1bd0c064ece83b204371
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 01:47:40 +0200

    [FEAT]: add login mutation.

--- packages/frontend/pages/auth/login.tsx
+++ packages/frontend/pages/auth/login.tsx
@@ -1,12 +1,42 @@
-import React from "react";
-import { Col, Layout, Row, Button } from "antd";
+import React, {useCallback} from "react";
+import {Button, Col, Layout, Row} from "antd";
 import AuthForm from "../../components/AuthForm";
-import { withApollo } from "../../lib/apollo";
+import {withApollo} from "../../lib/apollo";
 import Link from "next/link";
+import gql from "graphql-tag";
+import {useRouter} from "next/router";
+import {useMutation} from "@apollo/react-hooks";
+import {LoginData, LoginVars} from "../../types/auth.type";
 
 const { Content } = Layout;
 
+const LOGIN_MUTATION = gql`
+  mutation LOGIN($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      _id
+      email
+      token
+    }
+  }
+`;
+
 const LoginPage = () => {
+  const router = useRouter();
+  const [login, { loading, error, data }] = useMutation<LoginData, LoginVars>(
+    LOGIN_MUTATION,
+  );
+
+  if (data && data.login) {
+    localStorage.setItem("token", data.login.token);
+    console.log("Logged In");
+    // router.replace("/");
+  }
+
+  const onLogin = useCallback(
+    (variables: LoginVars) => login({ variables }).catch(e => e),
+    [login],
+  );
+
   return (
     <Layout style={{ height: "100vh" }}>
       <Content>
@@ -14,10 +44,11 @@ const LoginPage = () => {
           <Col span={8}>
             <AuthForm
               type="Login"
-              loading={false}
-              error={null}
+              loading={loading}
+              error={error}
               name="login"
               buttonText="LOGIN"
+              onSubmit={onLogin}
             />
             <Button type="link" block>
               <Link href="/auth/register">
