37c4b1738c301e147c38933011d710f025c91fec
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 00:45:03 +0200

    [FIX]: update apollo client uri.

--- packages/frontend/apolloClient.ts
+++ packages/frontend/apolloClient.ts
@@ -4,16 +4,13 @@ import { HttpLink } from "apollo-link-http";
 import { NextPageContext } from "next";
 
 export default function createApolloClient(
-  initialState: NormalizedCacheObject,
-  ctx: NextPageContext,
+    initialState: NormalizedCacheObject,
+    ctx: NextPageContext,
 ) {
-  // The `ctx` (NextPageContext) will only be present on the server.
-  // use it to extract auth headers (ctx.req) or similar.
   return new ApolloClient({
     ssrMode: Boolean(ctx),
     link: new HttpLink({
-      uri: "https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn", // Server URL (must be absolute)
-      credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
+      uri: "http://localhost:4000/graphql",
     }),
     cache: new InMemoryCache().restore(initialState),
   });
