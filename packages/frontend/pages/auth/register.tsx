6bfbfe8edbe88fcd5e34e03dec4f7bebca1c02a6
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 01:47:31 +0200

    [FEAT]: add register mutation.

--- packages/frontend/pages/auth/register.tsx
+++ packages/frontend/pages/auth/register.tsx
@@ -1,12 +1,43 @@
-import React from "react";
-import { Col, Layout, Row, Button } from "antd";
+import React, {useCallback} from "react";
+import {Button, Col, Layout, Row} from "antd";
 import AuthForm from "../../components/AuthForm";
-import { withApollo } from "../../lib/apollo";
+import {withApollo} from "../../lib/apollo";
 import Link from "next/link";
+import gql from "graphql-tag";
+import {RegisterData, RegisterVars} from "../../types/auth.type";
+import {useMutation} from "@apollo/react-hooks";
+import {useRouter} from "next/router";
 
 const { Content } = Layout;
 
+const REGISTER_MUTATION = gql`
+  mutation REGISTER($email: String!, $password: String!) {
+    register(email: $email, password: $password) {
+      _id
+      email
+      token
+    }
+  }
+`;
+
 const RegisterPage = () => {
+  const router = useRouter();
+  const [register, { loading, error, data }] = useMutation<
+    RegisterData,
+    RegisterVars
+  >(REGISTER_MUTATION);
+
+  if (data && data.register) {
+    localStorage.setItem("token", data.register.token);
+    console.log("Registered");
+    // router.replace("/");
+  }
+
+  const onRegister = useCallback(
+    (variables: RegisterVars) => register({ variables }).catch(e => e),
+    [register],
+  );
+
   return (
     <Layout style={{ height: "100vh" }}>
       <Content>
@@ -14,10 +45,11 @@ const RegisterPage = () => {
           <Col span={8}>
             <AuthForm
               type="Register"
-              loading={false}
-              error={null}
+              loading={loading}
+              error={error}
               name="register"
               buttonText="REGISTER"
+              onSubmit={onRegister}
             />
             <Button type="link" block>
               <Link href="/auth/login">
