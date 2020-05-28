b321a02e167532dccae7e159a61eba2d6efc8dc9
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 01:29:52 +0200

    [FEAT]: open on register page.

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,5 +1,14 @@
 import { withApollo } from "../lib/apollo";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-const IndexPage = () => <div>hello World</div>;
+const IndexPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/auth/register");
+  }, []);
+
+  return <div>Hello World!</div>;
+};
 
 export default withApollo({ ssr: true })(IndexPage);
