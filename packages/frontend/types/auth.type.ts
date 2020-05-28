5dd4bbd3259e9f33ac253cf239d8a94bbaf24e54
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 01:47:04 +0200

    [FEAT]: add auth types.

---
+++ packages/frontend/types/auth.type.ts
@@ -0,0 +1,8 @@
+export type RegisterData = {
+  register: { email: string; token: string; _id: string };
+};
+export type RegisterVars = { email: string; password: string };
+
+export type LoginData = { login: { email: string; token: string } };
+
+export type LoginVars = { email: string; password: string };
