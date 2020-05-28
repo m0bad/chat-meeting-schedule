ebfe0c172ed80e1b71d93f6d8091c3596f104a8f
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 01:29:13 +0200

    [FEAT]: add auth form component.

---
+++ packages/frontend/components/AuthForm.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Alert, Button, Card, Form, Input, Typography } from "antd";
+import { UnlockOutlined, UserOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
+
+const AuthForm = ({ name, loading, error, type, buttonText }) => {
+  return (
+    <Card bordered={false}>
+      {error &&
+        error.graphQLErrors.map(({ message }, i) => (
+          <div key={i}>
+            <Alert message={message} type="error" />
+            <br />
+          </div>
+        ))}
+      <Form name={name} initialValues={{ email: "", password: "" }}>
+        <Title level={4}>{type}</Title>
+
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: "Email is required." }]}
+        >
+          <Input type="email" placeholder="Email" prefix={<UserOutlined />} />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Password is required." }]}
+        >
+          <Input
+            type="password"
+            placeholder="Password"
+            prefix={<UnlockOutlined />}
+          />
+        </Form.Item>
+
+        <Form.Item noStyle>
+          <Button loading={loading} type="primary" block htmlType="submit">
+            {buttonText}
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
+
+export default AuthForm;
