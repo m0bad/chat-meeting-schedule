83f23ef4ee0bb5ef05ffe1d3190929583ce66302
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 00:21:01 +0200

    [FEAT]: add auth service.

---
+++ packages/backend/src/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import { UserService } from "../user/user.service";
+import { UserDto } from "../user/dto/user.dto";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcryptjs";
+import { AuthArgsDto } from "./dto/auth-args.dto";
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register({ email, password }: AuthArgsDto) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    return this.userService.create({
+      email,
+      password: hashedPassword,
+    });
+  }
+
+  async login(user: UserDto): Promise<string> {
+    const payload = { email: user.email };
+    return this.jwtService.sign(payload);
+  }
+
+  async validate(email: string, password: string): Promise<UserDto> {
+    const user = await this.userService.findOne(email);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
