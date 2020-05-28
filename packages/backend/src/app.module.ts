4efd64be4486e6bcfdd93fe53b3a094d15681d13
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 00:21:35 +0200

    [FIX]: modules dependencies issues.

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
 import { MongooseModule } from "@nestjs/mongoose";
 import {UserModule} from "./user/user.module";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,7 +16,8 @@ import {UserModule} from "./user/user.module";
       autoSchemaFile: true,
       playground: true,
     }),
-    UserModule
+    UserModule,
+    AuthModule
   ],
   providers: [AppService, AppResolver],
 })
---
+++ packages/backend/src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { AuthResolver } from "./auth.resolver";
+import { AuthService } from "./auth.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { UserModule } from "../user/user.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./helpers/jwt.strategy";
+
+@Module({
+  imports: [
+    ConfigModule,
+    UserModule,
+    PassportModule.register({ defaultStrategy: "jwt" }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get("SECRET"),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthResolver, AuthService, JwtStrategy],
+})
+export class AuthModule {}
