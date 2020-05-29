f20054b4df18fe0328a70b70e3e4dd493b20c94b
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 18:17:05 +0200

    [FEAT]: update user module to have chats.

--- packages/backend/src/user/user.schema.ts
+++ packages/backend/src/user/user.schema.ts
@@ -1,13 +1,26 @@
 import * as mongoose from "mongoose";
+import { Chat } from "../chat/chat.schema";
 
 export interface User extends mongoose.Document {
   email: string;
   username: string;
   password: string;
+  chats: Chat[];
 }
 
-export const UserSchema = new mongoose.Schema<User>({
-  email: String,
-  username: { type: String, unique: true },
-  password: String,
-});
+export const UserSchema = new mongoose.Schema<User>(
+  {
+    email: String,
+    username: { type: String, unique: true },
+    password: String,
+    chats: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Chat",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
