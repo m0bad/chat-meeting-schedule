8696be85c897abc0b307c7a7d12b0b189f5d3a16
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 04:25:10 +0200

    [FIX]: remove typeName property for graphql.

--- packages/frontend/apolloClient.ts
+++ packages/frontend/apolloClient.ts
@@ -4,14 +4,14 @@ import { HttpLink } from "apollo-link-http";
 import { NextPageContext } from "next";
 
 export default function createApolloClient(
-    initialState: NormalizedCacheObject,
-    ctx: NextPageContext,
+  initialState: NormalizedCacheObject,
+  ctx: NextPageContext,
 ) {
   return new ApolloClient({
     ssrMode: Boolean(ctx),
     link: new HttpLink({
       uri: "http://localhost:4000/graphql",
     }),
-    cache: new InMemoryCache().restore(initialState),
+    cache: new InMemoryCache({ addTypename: false }).restore(initialState),
   });
 }
