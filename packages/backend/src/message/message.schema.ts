58c7fbe12c189bbf0420cb5ba006355b08defae4
mobad <mostafa.m.badawey@gmail.com>
2020-05-29 18:16:51 +0200

    [FEAT]: add message module.

---
+++ packages/backend/src/message/message.schema.ts
@@ -0,0 +1,26 @@
+import * as mongoose from "mongoose";
+import { User } from "../user/user.schema";
+import { Chat } from "../chat/chat.schema";
+
+export interface Message extends mongoose.Document {
+  body: string;
+  sender: User;
+  chat: Chat;
+}
+
+export const MessageSchema = new mongoose.Schema<Message>(
+  {
+    body: String,
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
