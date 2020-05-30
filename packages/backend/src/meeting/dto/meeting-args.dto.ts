712c63f410a582f347c559fdd09fcf279ec64a43
mobad <mostafa.m.badawey@gmail.com>
2020-05-30 22:53:40 +0200

    [FEAT]: add meeting Args type.

---
+++ packages/backend/src/meeting/dto/meeting-args.dto.ts
@@ -0,0 +1,19 @@
+import { ArgsType, Field } from "@nestjs/graphql";
+
+@ArgsType()
+export class MeetingArgsType {
+  @Field(type => String)
+  type: string;
+
+  @Field(type => String)
+  location: string;
+
+  @Field(type => [String])
+  users: string[];
+
+  @Field(type => Date)
+  startDate: Date;
+
+  @Field(type => Date)
+  endDate: Date;
+}
