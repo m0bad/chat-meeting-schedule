0af914c0901cd923f671a8c01889b646a53b07a4
mobad <mostafa.m.badawey@gmail.com>
2020-05-30 05:52:53 +0200

    [FEAT]: add new Message logic.

--- packages/frontend/components/MessageInput.tsx
+++ packages/frontend/components/MessageInput.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Input } from "antd";
 import { SendOutlined } from "@ant-design/icons";
 
 const { TextArea } = Input;
 
-export const MessageInput = () => (
-  <div className="input-field">
-    <TextArea rows={2} className="text-area" onPressEnter={null} />
-    <span className="suffix">
-      <span className="send">
-        <SendOutlined style={{ color: "green", fontSize: "22px" }} />
-      </span>
-    </span>
-  </div>
-);
+export const MessageInput = ({ onSubmit }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      onSubmit(e.target.value || value);
+      setValue("");
+    },
+    [onSubmit],
+  );
+
+  const handleChange = e => setValue(e.target.value);
+
+  return (
+    <div className="input-field">
+      <TextArea
+        rows={2}
+        className="text-area"
+        onPressEnter={handleSubmit}
+        value={value}
+        onChange={handleChange}
+      />
+      {/*<span className="suffix">*/}
+      {/*  <span className="send" onClick={handleSubmit}>*/}
+      {/*    <SendOutlined style={{ color: "green", fontSize: "22px" }} />*/}
+      {/*  </span>*/}
+      {/*</span>*/}
+    </div>
+  );
+};
