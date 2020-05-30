9aa69eda3a21487990705296fe9e59ae3363ae7e
mobad <mostafa.m.badawey@gmail.com>
2020-05-30 05:52:12 +0200

    [FEAT]: users list component.

---
+++ packages/frontend/components/UsersList.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback } from "react";
+import { Avatar, Menu } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+const { SubMenu } = Menu;
+
+const UsersList = ({ data, onClick, key, title, ...otherProps }) => {
+  const handleClick = useCallback(
+    user => {
+      const loggedInUser = JSON.parse(localStorage.getItem("user"));
+      console.log({ user, loggedInUser });
+      onClick(user);
+    },
+    [onClick],
+  );
+
+  return (
+    <SubMenu key={key} icon={<UserOutlined />} title={title} {...otherProps}>
+      {data &&
+        data.map(user => (
+          <Menu.Item
+            key={user._id}
+            icon={
+              <Avatar
+                style={{ backgroundColor: "#87d068", margin: "5px" }}
+                icon={<UserOutlined />}
+              />
+            }
+            onClick={() => handleClick(user)}
+          >
+            {user.username}
+          </Menu.Item>
+        ))}
+    </SubMenu>
+  );
+};
+
+export default UsersList;
