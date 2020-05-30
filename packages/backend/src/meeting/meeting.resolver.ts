2eba5de55390cec0493c812f32133763905b66e4
mobad <mostafa.m.badawey@gmail.com>
2020-05-30 22:54:03 +0200

    [FEAT]: add meeting resolver.

---
+++ packages/backend/src/meeting/meeting.resolver.ts
@@ -0,0 +1,20 @@
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { MeetingArgsType } from "./dto/meeting-args.dto";
+import { MeetingDto } from "./dto/meeting.dto";
+import { Meeting } from "./meeting.schema";
+import { MeetingService } from "./meeting.service";
+
+@Resolver("Meeting")
+export class MeetingResolver {
+  constructor(private readonly meetingService: MeetingService) {}
+
+  @Query(returns => [MeetingDto])
+  async meetings(@Args("user") user: string): Promise<MeetingDto[]> {
+    return this.meetingService.getMeetings(user);
+  }
+
+  @Mutation(returns => MeetingDto)
+  async scheduleMeeting(@Args() args: MeetingArgsType): Promise<MeetingDto> {
+    return this.meetingService.createMeeting(args);
+  }
+}
