5c0b1fdc7145a50f32b0a7ec8af13d540be64151
mobad <mostafa.m.badawey@gmail.com>
2020-05-30 18:33:15 +0200

    [REFACTOR]: messagesList to an external component.

---
+++ packages/frontend/components/MessagesList.tsx
@@ -0,0 +1,31 @@
+import { Message } from "./Message";
+import React from "react";
+import { Spin } from "antd";
+
+export const MessagesList = ({ data, loggedInUser, loading }) => (
+  <>
+    {loading ? (
+      <div className="loading-spinner">
+        <Spin />
+      </div>
+    ) : (
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          margin: 0,
+          padding: 0,
+        }}
+      >
+        {data &&
+          data?.messages.map(message => (
+            <Message
+              isMe={message.sender === loggedInUser}
+              body={message.body}
+              key={message._id}
+            />
+          ))}
+      </ul>
+    )}
+  </>
+);
