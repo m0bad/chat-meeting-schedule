0f131b4bcde8f6fb7236c690605b5fbec11e0b67
mobad <mostafa.m.badawey@gmail.com>
2020-05-31 09:34:28 +0200

    [FEAT]: add new meetings subscription.

--- packages/backend/src/meeting/meeting.resolver.ts
+++ packages/backend/src/meeting/meeting.resolver.ts
@@ -1,8 +1,12 @@
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver, Subscription } from "@nestjs/graphql";
 import { MeetingArgsType } from "./dto/meeting-args.dto";
 import { MeetingDto, MeetingResultDto } from "./dto/meeting.dto";
 import { Meeting } from "./meeting.schema";
 import { MeetingService } from "./meeting.service";
+import { PubSub } from "apollo-server-express";
+import {MeetingsUnAvailableTimesDto} from "./dto/meeting-unavailable-times.dto";
+
+const pubSub = new PubSub();
 
 @Resolver("Meeting")
 export class MeetingResolver {
@@ -13,13 +17,23 @@ export class MeetingResolver {
     return this.meetingService.getMeetings(user);
   }
 
-  @Query(returns => [[Date]])
-  async unavailableTimes(@Args("user") user: string): Promise<Date[][]> {
+  @Query(returns => [MeetingsUnAvailableTimesDto])
+  async unavailableTimes(@Args("user") user: string): Promise<any> {
     return this.meetingService.getUnavailableTimes(user);
   }
 
   @Mutation(returns => MeetingDto)
   async scheduleMeeting(@Args() args: MeetingArgsType): Promise<MeetingDto> {
-    return this.meetingService.createMeeting(args);
+    const meeting = await this.meetingService.createMeeting(args);
+    await pubSub.publish("newMeeting", meeting);
+    return meeting;
+  }
+
+  @Subscription(returns => MeetingDto, {
+    resolve: payload => payload,
+    filter: (payload, variables) => payload.users.includes(variables.user),
+  })
+  newMeeting(@Args("user") user: string) {
+    return pubSub.asyncIterator("newMeeting");
   }
 }
