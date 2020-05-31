bddae6a9fea16ef35ef69ac8775def7c966373ca
mobad <mostafa.m.badawey@gmail.com>
2020-05-31 09:33:15 +0200

    [FEAT]: add unavailable times type.

---
+++ packages/backend/src/meeting/dto/meeting-unavailable-times.dto.ts
@@ -0,0 +1,10 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+
+@ObjectType()
+export class MeetingsUnAvailableTimesDto {
+  @Field(type => String)
+  date: string;
+
+  @Field(type => [Number])
+  hours: number[];
+}
