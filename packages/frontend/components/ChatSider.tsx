0672ef94e775b30807a667fdb889ed788c6eb726
mobad <mostafa.m.badawey@gmail.com>
2020-05-31 10:03:48 +0200

    [REFACTOR]: chat sider to it's new component.

---
+++ packages/frontend/components/ChatSider.tsx
@@ -0,0 +1,54 @@
+import UserAvatar from "./UserAvatar";
+import { Button, Divider, Layout, Menu } from "antd";
+import UsersList from "./UsersList";
+import { MeetingsList } from "./MeetingsList";
+import React, { useState } from "react";
+
+const { Sider } = Layout;
+
+export const ChatSider = ({
+  loggedInUser,
+  data,
+  loading,
+  setSelectedUser,
+  userMeetings,
+  onLogOut,
+}) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={() => setCollapsed(!collapsed)}
+      theme="light"
+      width="20vw"
+      style={{ position: "relative" }}
+    >
+      <UserAvatar
+        username={loggedInUser?.username}
+        textColor="#4FC3F7"
+        size="large"
+      />
+      <Divider />
+      <Menu mode="inline">
+        <UsersList
+          data={data?.users}
+          loading={loading}
+          onClick={setSelectedUser}
+          key="users"
+          title="Users"
+        />
+        <MeetingsList
+          meetings={userMeetings?.meetings}
+          loggedInUser={loggedInUser}
+        />
+        <Menu.Item style={{ position: "absolute", bottom: "3rem" }}>
+          <Button type="primary" danger block onClick={onLogOut}>
+            LOGOUT
+          </Button>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
+};
--- packages/frontend/pages/chat.tsx
+++ packages/frontend/pages/chat.tsx
@@ -1,17 +1,13 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useQuery, useSubscription } from "@apollo/react-hooks";
-import { Button, Divider, Layout, Menu } from "antd";
+import { Layout } from "antd";
 import { withApollo } from "../lib/apollo";
-import UsersList from "../components/UsersList";
 import { ChatWindow } from "../components/ChatWindow";
 import { useRouter } from "next/router";
 import { USERS_QUERY } from "../graphql/chat/chat.query";
-import UserAvatar from "../components/UserAvatar";
-import { MeetingsList } from "../components/MeetingsList";
 import { MEETINGS_QUERY } from "../graphql/meeting/meeting.query";
 import gql from "graphql-tag";
-
-const { Sider } = Layout;
+import { ChatSider } from "../components/ChatSider";
 
 export const MEETINGS_SUBSCRIPTION = gql`
   subscription newMeeting($user: String!) {
@@ -28,7 +24,6 @@ export const MEETINGS_SUBSCRIPTION = gql`
 
 const ChatPage = () => {
   const router = useRouter();
-  const [collapsed, setCollapsed] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState(null);
   const [selectedUser, setSelectedUser] = useState(null);
 
@@ -62,39 +57,14 @@ const ChatPage = () => {
   });
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={() => setCollapsed(!collapsed)}
-        theme="light"
-        width="20vw"
-        style={{ position: "relative" }}
-      >
-        <UserAvatar
-          username={loggedInUser?.username}
-          textColor="#4FC3F7"
-          size="large"
-        />
-        <Divider />
-        <Menu mode="inline">
-          <UsersList
-            data={data?.users}
-            loading={loading}
-            onClick={setSelectedUser}
-            key="users"
-            title="Users"
-          />
-          <MeetingsList
-            meetings={userMeetings?.meetings}
-            loggedInUser={loggedInUser}
-          />
-          <Menu.Item style={{ position: "absolute", bottom: "3rem" }}>
-            <Button type="primary" danger block onClick={onLogOut}>
-              LOGOUT
-            </Button>
-          </Menu.Item>
-        </Menu>
-      </Sider>
+      <ChatSider
+        loggedInUser={loggedInUser}
+        data={data}
+        loading={loading}
+        setSelectedUser={setSelectedUser}
+        userMeetings={userMeetings}
+        onLogOut={onLogOut}
+      />
       {selectedUser && (
         <ChatWindow
           selectedUser={selectedUser}
