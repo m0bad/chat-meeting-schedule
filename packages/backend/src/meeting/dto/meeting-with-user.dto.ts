ffc1fb0a05e3b6f443765bf263ddcd2c8e83576c
mobad <mostafa.m.badawey@gmail.com>
2020-05-31 02:10:50 +0200

    [FEAT]: meeting with user dto.

---
+++ packages/backend/src/meeting/dto/meeting-with-user.dto.ts
@@ -0,0 +1,24 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+import { MeetingTypeEnum } from "./meeting-type.enum";
+import { UserDto } from "../../user/dto/user.dto";
+
+@ObjectType()
+export class MeetingWithUserDto {
+  @Field()
+  _id: string;
+
+  @Field(type => MeetingTypeEnum)
+  type: MeetingTypeEnum;
+
+  @Field(type => String)
+  location: string;
+
+  @Field(type => [UserDto])
+  users: UserDto[];
+
+  @Field(type => Date)
+  startDate: Date;
+
+  @Field(type => Date)
+  endDate: Date;
+}
